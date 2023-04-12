a539e07125082aa4bb071be01a4b33ac32c4fbbc
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2023-04-12 11:08:50 -0300

    Organização do template e layout

--- condominio/urls.py
+++ condominio/urls.py
@@ -1,4 +1,8 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 from condominio import views
 
@@ -10,4 +14,9 @@ urlpatterns = [
     path('update/<int:pk>/', views.CondominioUpdate.as_view(), name='update'),
     path('detail/<int:pk>/', views.CondominioDetail.as_view(), name='detail'),
     path('delete/<int:pk>/', views.CondominioDelete.as_view(), name='delete'),
+
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico')))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
