e26acd5671653811b32ba9b6fd2d6f63af93203e
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2023-04-22 21:53:43 -0300

    ajustes

--- condominio/models.py
+++ condominio/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Condominio(models.Model):
     nome = models.CharField(max_length=200)
     documento = models.CharField('CNPJ', max_length=20)
@@ -10,12 +11,12 @@ class Condominio(models.Model):
     cidade = models.CharField(max_length=50)
     estado = models.CharField(max_length=50)
     pais = models.CharField('País', max_length=50)
-    area_comum = models.CharField('área comum', max_length=50, blank=True, null=True)
-    area_privativa = models.CharField('área privativa', max_length=50, blank=True, null=True)
-    area_total = models.CharField('área total', max_length=50, blank=True, null=True)
-    
-    dia_vencimento_boleto = models.CharField('Dia de vencimento da fatura', max_length=2)
-    
+    area_comum = models.CharField('área comum', max_length=50, blank=True, null=True) # noqa
+    area_privativa = models.CharField('área privativa', max_length=50, blank=True, null=True) # noqa
+    area_total = models.CharField('área total', max_length=50, blank=True, null=True) # noqa
+
+    dia_vencimento_boleto = models.CharField('Dia de vencimento da fatura', max_length=2) # noqa
+
     status = models.BooleanField(default=True, blank=True, null=True)
     data_inicio_contrato = models.DateTimeField(blank=True, null=True)
     data_fim_contrato = models.DateTimeField(blank=True, null=True)
@@ -25,11 +26,12 @@ class Condominio(models.Model):
 
     def __str__(self):
         return self.nome
-    
+
+
 class Unidade(models.Model):
     nome = models.CharField(max_length=200)
     fracao = models.CharField('fração', max_length=20)
-    condominio = models.ForeignKey(Condominio, on_delete=models.CASCADE, related_name='unidades')
+    condominio = models.ForeignKey(Condominio, on_delete=models.CASCADE, related_name='unidades') # noqa
 
 
 class Pessoa(models.Model):
@@ -40,13 +42,13 @@ class Pessoa(models.Model):
 
     nome = models.CharField(max_length=200)
     documento = models.CharField('CPF', max_length=20)
-    status = models.CharField(choices=Status.choices, default=Status.ATIVO, max_length=10)
+    status = models.CharField(choices=Status.choices, default=Status.ATIVO, max_length=10) # noqa
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.nome
-    
+
 
 class PessoaUnidade(models.Model):
 
@@ -54,11 +56,11 @@ class PessoaUnidade(models.Model):
         PROPRIETARIO = 'Proprietário', 'Proprietário'
         LOCATARIO = 'Locatário', 'Locatário'
 
-    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE, related_name="morador")
-    unidade = models.ForeignKey(Unidade, on_delete=models.PROTECT, related_name="morador")
-    tipo = models.CharField(choices=Morador.choices, default=Morador.PROPRIETARIO, max_length=15)
-    data_inicio = models.DateTimeField('data de início',blank=True, null=True)
-    data_fim = models.DateTimeField('data de fim',blank=True, null=True)
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE, related_name="morador") # noqa
+    unidade = models.ForeignKey(Unidade, on_delete=models.PROTECT, related_name="morador") # noqa
+    vinculo = models.CharField(choices=Morador.choices, default=Morador.PROPRIETARIO, max_length=15) # noqa
+    data_inicio = models.DateTimeField('data de início', blank=True, null=True)
+    data_fim = models.DateTimeField('data de fim', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
